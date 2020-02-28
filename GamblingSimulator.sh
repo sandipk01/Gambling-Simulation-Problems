c67e1c12815f00ea2ea5c17a11e4079043a004a7
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-28 18:59:34 +0530

    REFACTORED : betting win or loose Random function is added

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,12 +4,27 @@
 #bets every game to win or loose some stake. Being a Calculative
 #Gambler exits if the Stake reaches a high or a low limit
 
-#VARIABLES
-everyGameBet=1
+
 
 #CONSTANTS
 IS_WIN=1
 IS_LOOSE=0
 STAKE=100
 
+#VARIABLES
+everyDayStake=100
+everyGameBet=1
+randomNumber=0
 
+randomNumber=$(( RANDOM % 2 ))
+
+if [ $randomNumber -eq $IS_WIN  ]
+	then
+		printf "win: $everyGameBet\n"
+		everyDayStake=$(( $everyDayStake + $everyGameBet ))
+	else
+		everyDayStake=$(( $everyDayStake - $everyGameBet ))
+		printf "loose:$everyGameBet\n"
+fi
+
+printf "Total Money: $everyDayStake\n"
