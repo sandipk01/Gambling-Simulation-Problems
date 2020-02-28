21fa875d8cfca02a94532fa1755f26d3c548f10f
sadnipk01 <sadnipkenagar01@gmail.com>
2020-02-28 18:54:12 +0530

    ADDED : GamblingSimulator.sh added and vaiables and constants are initialized

---
+++ GamblingSimulator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+#This problem simulates a Gambler who starts with a stake and
+#bets every game to win or loose some stake. Being a Calculative
+#Gambler exits if the Stake reaches a high or a low limit
+
+#VARIABLES
+everyGameBet=1
+
+#CONSTANTS
+IS_WIN=1
+IS_LOOSE=0
+STAKE=100
+
+
