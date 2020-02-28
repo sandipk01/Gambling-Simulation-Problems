8bea316f00bbfba40a2a2bf49c8f405e3686fb25
sandipk01 <sandipkengar01@gmail.com>
2020-02-29 04:58:39 +0530

    REFACTORED : Changed daily win and loosing logic decrease the iteration

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,43 +4,31 @@
 #bets every game to win or loose some stake. Being a Calculative
 #Gambler exits if the Stake reaches a high or a low limit
 
-
-
-everyGameBet=1
 #CONSTANTS
 IS_WIN=1
 IS_LOOSE=0
 STAKE=100
 IS_RESIGN=0
-
+declare -A array
 #VARIABLES
-randomNumber=0
-totalStakeDay=0
+everyGameBet=1
 
-function play()
-{
 halfStake=$(( $STAKE / 2 ))
 winLimit=$(( ( $STAKE + $halfStake ) - $STAKE ))
 looseLimit=$(( ( $STAKE - $halfStake ) - $STAKE ))
+totalStakeDay=0
 
-echo $winLimit $looseLimit
-
-while [ $IS_RESIGN == 0 ]
+for (( index=1; index<=20; index++ ))
 do
-   randomNumber=$(( RANDOM % 2 ))
-   if [ $randomNumber -eq $IS_WIN  ]
-      then
-         totalStakeDay=$(( $totalStakeDay + $everyGameBet ))
-      else
-         totalStakeDay=$(( $totalStakeDay - $everyGameBet ))
-   fi
-      if [ $totalStakeDay -ge $winLimit -o $totalStakeDay -le $looseLimit ]
-         then
-            printf "Resign for the day\n"
-            IS_RESIGN=1
-      fi
-done
-  echo $totalStakeDay
-}
 
-printf "$( play )"
+randomNumber=$(( RANDOM%2 ))
+if [ $randomNumber -eq $IS_WIN ]
+	then
+		totalStakeDay=$winLimit
+		result="winning"
+	else
+		totalStakeDay=$looseLimit
+		result="loosing"
+fi
+echo $index "day: "  $totalStakeDay "$result"
+done
